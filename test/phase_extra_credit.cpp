9e2c4ac31e63167991ca4c64c758598c05835786
Noah I <noah.imam@gmail.com>
2018-05-12 00:25:43 -0500

    restoring original phase_extra

--- test/phase_extra_credit.cpp
+++ test/phase_extra_credit.cpp
@@ -2,38 +2,14 @@
 #include <atomic>
 #include <thread>
 #include <vector>
-#include <mutex>
-#include <pthread.h>
-#include "common.h"
 
-struct Race {
-  uint32_t count;
+constexpr int const kKernelThreads = 50;
 
-  Race() { 
-    count = 0xdeadbeef;
-  }
-};
-
-static Race *race = NULL;
-
-void Create(void*) {
-  race = new Race();
-}
-
-void Ready(void*) {
-  if (race) {
-     ASSERT(race->count == 0xdeadbeef);
-  }
-}
-
-constexpr int const kKernelThreads = 2;
+void GreenThreadWorker(void*) { printf("Green thread on kernel thread.\n"); }
 
 void KernelThreadWorker(int n) {
   chloros::Initialize();
-  if (n%2 == 0)
-    chloros::Spawn(Create, nullptr);
-  else
-    chloros::Spawn(Ready, nullptr);
+  chloros::Spawn(GreenThreadWorker, nullptr);
   chloros::Wait();
   printf("Finished thread %d.\n", n);
 }
@@ -46,9 +22,5 @@ int main() {
   for (int i = 0; i < kKernelThreads; ++i) {
     threads[i].join();
   }
-
-  if (race)
-    delete race;
-
   return 0;
-} 
+}
